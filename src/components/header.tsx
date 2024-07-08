30d71c4434d3a7484366d3629f69bccfb8eca649
d_yermakov <gntnit@gmail.com>
2024-07-08 12:26:49 +0300

    add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
+
 export default function Header() {
   return (
     <>
       <header>
-        Header
+        <div className="logo-container">
+          <Image alt="logo" height={100} src="/images/logo.svg" width={100} />
+        </div>
       </header>
     </>
   )
