af3552cbf268fafa944b96d2ef208572784c67b9
d_yermakov <gntnit@gmail.com>
2024-07-10 11:40:15 +0300

    fix header bg color

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <>
       <header className="w-full bg-custom-blue">
-        <nav className="bg-blue-200">
+        <nav className="">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
