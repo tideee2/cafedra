95e90ab73fbcef168369e843a1fbcfde068968bc
d_yermakov <gntnit@gmail.com>
2024-07-12 08:30:25 +0300

    add footer updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      fontFamily: {
+        Calibri: ['Calibri', 'sans-serif'],
+      },
       backgroundSize: {
         'auto': 'auto',
         'cover': 'cover',
