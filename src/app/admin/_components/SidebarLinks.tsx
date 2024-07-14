4c95997c4f5c9ec2d04bc592c5ee81346bd4e049
d_yermakov <gntnit@gmail.com>
2024-07-14 08:59:53 +0300

    add active admin page indicator

--- src/app/admin/_components/SidebarLinks.tsx
+++ src/app/admin/_components/SidebarLinks.tsx
@@ -2,17 +2,20 @@
 
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import styles from './styles.module.scss'
 import { headerLinks } from '@/constants/header'
-import styles from '@/components/header/styles.module.scss'
 
 export default function SidebarLinks() {
   const pathname = usePathname()
-  const linkStyle = 'flex items-center w-full p-3 rounded-lg text-start leading-tight font-medium transition-all hover:text-green focus:text-green active:text-blue-900 outline-none'
+  const linkStyle = 'flex items-center w-full p-3 text-start leading-tight font-medium transition-all hover:text-green focus:text-green active:text-blue-900 outline-none'
+  const isActive = (link: string) => {
+    return pathname === `/admin${link}` || (pathname === '/admin' && link === '/')
+  }
   return (
     headerLinks.map(headerLink => (
       <Link
         aria-current="page"
-        className={`${linkStyle} ${pathname === headerLink.href ? styles.activeLink : ''}`}
+        className={`${linkStyle} ${isActive(headerLink.href) ? styles.activeLink : ''}`}
         href={`/admin${headerLink.href}`}
         key={headerLink.href}
         tabIndex={0}
