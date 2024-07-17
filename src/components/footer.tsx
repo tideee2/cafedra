491d68a7a043fae47199e5868ac8bc79c2bbcfaa
d_yermakov <gntnit@gmail.com>
2024-07-17 12:58:04 +0300

    footer fixes 1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -35,30 +35,32 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="bg-white text-teal-900 Calibri w-full font-bold flex flex-row h-24 justify-around items-center  ">
-        <div className=" lg:basis-1/3 text-xs flex flex-row items-center justify-around sm:mt-3">
-          <div className="flex flex-row items-center justify-between uppercase">
-            <Image alt="Icon email" height={5} priority src="/images/1.png" width={10} />
-            ну львівська політехніка 1816-
-            {year}-{month}-{day} {hours}:{minutes}:{seconds}
+      <footer className="bg-white text-teal-900 Calibri w-full font-bold h-24 sm:flex flex-column  justify-around items-center space-y-2 lg:flex flex-row ">
+        <div className=" basis-1/3 text-xs flex flex-row items-center justify-around ">
+          <div className="flex flex-row items-center justify-center uppercase">
+            <Image alt="Icon" height={5} priority src="/images/1.png" width={10} />
+            <div className="ml-2">
+              ну львівська політехніка 1816-
+              {year}-{month}-{day} {hours}:{minutes}:{seconds}
+            </div>
           </div>
         </div>
 
-        <div className="basis-2/4 text-xs flex flex-row justify-between">
+        <div className="basis-2/4 text-xs sm:flex flex-column justify-center space-x-4 text-center lg:flex flex-row items-center">
 
-          <div className="flex flex-row justify-around basis-4/12 ml-2 items-center">
+          <div className="flex flex-row justify-center items-center basis-4/12">
             <Image alt="Icon email" height={5} priority src="/images/icon_mail.png" width={17} />
-            <div className="mr-5 uppercase">coffice@lpnu.ua</div>
+            <div className="uppercase lg:ml-3 md:ml-1">coffice@lpnu.ua</div>
           </div>
 
-          <div className="flex flex-row justify-around basis-4/12 ml-2 items-center">
+          <div className="flex flex-row justify-center basis-4/12 items-center">
             <Image alt="Icon phone" height={5} priority src="/images/icon_phone.png" width={15} />
-            <div className="mr-5">+38 032 258-22-82</div>
+            <div className="lg:ml-3 md:ml-1">+38 032 258-22-82</div>
           </div>
 
-          <div className="flex flex-row justify-around basis-6/12 items-center">
+          <div className="flex flex-row justify-center basis-6/12 items-center">
             <Image alt="Icon location" height={4} priority src="/images/icon_addres.png" width={17} />
-            <div className="mr-3 uppercase">вул.C.Бандери, 12, Львів, Україна</div>
+            <div className="uppercase lg:ml-3 md:ml-1">вул.C.Бандери, 12, Львів, Україна</div>
           </div>
 
         </div>
