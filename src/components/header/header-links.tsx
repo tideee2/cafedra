92beaf7dcb0f77c6c27c31d4b04af81dcae860e5
d_yermakov <gntnit@gmail.com>
2024-07-17 17:52:14 +0300

    fix: highlight active link

--- src/components/header/header-links.tsx
+++ src/components/header/header-links.tsx
@@ -6,10 +6,13 @@ import styles from '@/components/header/styles.module.scss'
 export default function HeaderLinks() {
   const pathname = usePathname()
   const linkStyle = 'flex align-items-center px-1 lg:px-3 py-2 text-sm font-medium hover:text-green whitespace-nowrap border-b-2-transparent'
+  const isActiveLink = (link: string, path: string) => {
+    return path === `${link}` || (path === '/' && link === '/') || (`/${path.split('/')[1]}` === `${link}`)
+  }
   return headerLinks.map(link => (
     <Link
       aria-current="page"
-      className={`${linkStyle} ${pathname === link.href ? styles.activeLink : ''}`}
+      className={`${linkStyle} ${isActiveLink(link.href, pathname) ? styles.activeLink : ''}`}
       href={link.href}
       key={link.href}
     >{ link.title }
