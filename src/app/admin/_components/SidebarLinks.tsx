42de80398f287cf8d0253d61683c4f5d386c3064
d_yermakov <gntnit@gmail.com>
2024-07-20 01:17:24 +0300

    fix highlight links for sidebar admin

--- src/app/admin/_components/SidebarLinks.tsx
+++ src/app/admin/_components/SidebarLinks.tsx
@@ -8,14 +8,14 @@ import { headerLinks } from '@/constants/header'
 export default function SidebarLinks() {
   const pathname = usePathname()
   const linkStyle = 'flex items-center w-full p-3 text-start leading-tight font-medium transition-all hover:text-green focus:text-green active:text-blue-900 outline-none'
-  const isActive = (link: string) => {
-    return pathname === `/admin${link}` || (pathname === '/admin' && link === '/')
+  const isActive = (link: string, path: string) => {
+    return (path.includes(`admin${link}`) && link !== '/') || (link === '/' && !path.includes(`admin${link}`))
   }
   return (
     headerLinks.map(headerLink => (
       <Link
         aria-current="page"
-        className={`${linkStyle} ${isActive(headerLink.href) ? styles.activeLink : ''}`}
+        className={`${linkStyle} ${isActive(headerLink.href, pathname) ? styles.activeLink : ''}`}
         href={`/admin${headerLink.href}`}
         key={headerLink.href}
         tabIndex={0}
