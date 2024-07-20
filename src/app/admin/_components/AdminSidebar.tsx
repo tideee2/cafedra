d41474f51c35e6d7b2a7620893abd276a58416e0
d_yermakov <gntnit@gmail.com>
2024-07-20 23:24:18 +0300

    fix error with console errors after logout

--- src/app/admin/_components/AdminSidebar.tsx
+++ src/app/admin/_components/AdminSidebar.tsx
@@ -10,26 +10,26 @@ import { IsLoggedlocalStorageKey } from '@/constants/auth'
 export default function AdminSidebar() {
   const router = useRouter()
   const onLogout = () => {
-    deleteCookie(IsLoggedlocalStorageKey)
-    router.push('/')
+    setTimeout(() => {
+      deleteCookie(IsLoggedlocalStorageKey)
+      window.location.href = '/'
+    }, 30)
   }
   return (
-    <>
-      <div
-        className="relative flex flex-col bg-clip-border bg-white text-gray-700 h-[calc(100vh)] w-full p-4"
-      >
-        <div className="mb-2 p-4">
-          <HeaderLogo className="flex flex-col text-center justify-center gap-3" type="sidebar" />
-        </div>
-        <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
-          <SidebarLinks />
-        </nav>
-        <div className="flex-grow">
-        </div>
-        <button className="flex gap-2 items-center cursor-pointer hover:text-green" onClick={onLogout}>
-          <LogoutIcon className="size-8" /><span className="text-lg">Вийти</span>
-        </button>
+    <div
+      className="relative flex flex-col bg-clip-border bg-white text-gray-700 h-[calc(100vh)] w-full p-4"
+    >
+      <div className="mb-2 p-4">
+        <HeaderLogo className="flex flex-col text-center justify-center gap-3" type="sidebar" />
       </div>
-    </>
+      <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+        <SidebarLinks />
+      </nav>
+      <div className="flex-grow">
+      </div>
+      <button className="flex gap-2 items-center cursor-pointer hover:text-green" onClick={onLogout}>
+        <LogoutIcon className="size-8" /><span className="text-lg">Вийти</span>
+      </button>
+    </div>
   )
 }
