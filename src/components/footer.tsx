0a1b62204d7640fb9e5ea199097623a81dae38d5
Teacher <teacher.informatics26@gmail.com>
2024-07-23 23:28:43 +0300

    Change file

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -35,35 +35,35 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="bg-white text-teal-900 Calibri w-full font-bold h-24 sm:flex flex-column  justify-around items-center space-y-2 lg:flex flex-row ">
-        <div className=" basis-1/3 text-xs flex flex-row items-center justify-around ">
-          <div className="flex flex-row items-center justify-center uppercase">
+      <footer className="bg-white w-full  h-24  items-center flex flex-row  justify-around space-x-10">
+        <div className=" basis-3/5 flex flex-row items-center justify-between text-teal-900 Calibri text-xs font-bold ">
+          <div className="flex flex-row items-center justify-center uppercase basis-9/12">
             <Image alt="Icon" height={5} priority src="/images/1.png" width={10} />
             <div className="ml-2">
               ну львівська політехніка 1816-
               {year}-{month}-{day} {hours}:{minutes}:{seconds}
             </div>
           </div>
-        </div>
 
-        <div className="basis-2/4 text-xs sm:flex flex-column justify-center space-x-4 text-center lg:flex flex-row items-center">
-
-          <div className="flex flex-row justify-center items-center basis-4/12">
+          <div className="flex flex-row justify-center items-center mr-2 basis-1/5 ">
             <Image alt="Icon email" height={5} priority src="/images/icon_mail.png" width={17} />
             <div className="uppercase lg:ml-3 md:ml-1">coffice@lpnu.ua</div>
           </div>
+        </div>
 
-          <div className="flex flex-row justify-center basis-4/12 items-center">
+        <div className="flex flex-row items-center space-x-3  text-teal-900 Calibri text-xs font-bold basis-6/12 mr-2">
+          <div className="flex flex-row justify-center items-center basis-4/12">
             <Image alt="Icon phone" height={5} priority src="/images/icon_phone.png" width={15} />
             <div className="lg:ml-3 md:ml-1">+38 032 258-22-82</div>
           </div>
 
-          <div className="flex flex-row justify-center basis-6/12 items-center">
+          <div className="flex flex-row justify-center basis-6/12 items-center ml-10">
             <Image alt="Icon location" height={4} priority src="/images/icon_addres.png" width={17} />
             <div className="uppercase lg:ml-3 md:ml-1">вул.C.Бандери, 12, Львів, Україна</div>
           </div>
 
         </div>
+
       </footer>
     </>
   )
