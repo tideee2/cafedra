f1af360e2c2db4ef650ce6af443436117c4ce7f8
d_yermakov <gntnit@gmail.com>
2024-07-24 23:57:18 +0300

    fix scrolling for admin sidebar

--- src/app/admin/_components/AdminSidebar.tsx
+++ src/app/admin/_components/AdminSidebar.tsx
@@ -17,19 +17,21 @@ export default function AdminSidebar() {
   }
   return (
     <div
-      className="relative flex flex-col bg-clip-border bg-white text-gray-700 h-[calc(100vh)] w-full p-4"
+      className="relative flex flex-col bg-clip-border bg-white text-gray-700 h-[calc(100%)] w-full p-4"
     >
-      <div className="mb-2 p-4">
-        <HeaderLogo className="flex flex-col text-center justify-center gap-3" type="sidebar" />
+      <div className="h-[calc(100vh_-_44px)] flex flex-col fixed bg-clip-border top-2 left-2">
+        <div className="mb-2 p-4">
+          <HeaderLogo className="flex flex-col text-center justify-center gap-3" type="sidebar" />
+        </div>
+        <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+          <SidebarLinks />
+        </nav>
+        <div className="flex-grow">
+        </div>
+        <button className="flex gap-2 items-center cursor-pointer hover:text-green" onClick={onLogout}>
+          <LogoutIcon className="size-8" /><span className="text-lg">Вийти</span>
+        </button>
       </div>
-      <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
-        <SidebarLinks />
-      </nav>
-      <div className="flex-grow">
-      </div>
-      <button className="flex gap-2 items-center cursor-pointer hover:text-green" onClick={onLogout}>
-        <LogoutIcon className="size-8" /><span className="text-lg">Вийти</span>
-      </button>
     </div>
   )
 }
