21df17cd352cd63aff8320c51884493ac3d0d198
d_yermakov <gntnit@gmail.com>
2024-07-25 14:58:50 +0300

    fix: default page lang, meta tags

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n: {
+        locales: ['uk', 'uk-UA'], // order does not matter here
+        defaultLocale: 'uk', // the default locale to fall back to
+        localeDetection: false // Disable automatic locale detection on `/`
+    },
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang="ua">
+    <html lang="uk">
       <body className={inter.className}>
         { children }
       </body>
