7645d31261f37cd4da7aee926f977dd347587b7b
Teacher <teacher.informatics26@gmail.com>
2024-07-26 13:33:44 +0300

    Change breakpoints properties

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -35,8 +35,8 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="bg-white  w-full h-24  text-teal-900 Calibri text-xs font-bold items-center sm:flex flex-column  justify-around  ld:flex flex-row ">
-        <div className=" basis-2/3 flex flex-row items-center justify-around sm: mt-8 mr-3  mb-5 lg:m-0">
+      <footer className="bg-white  w-full h-24  text-black Calibri font-bold items-center text-9px sm:flex flex-column justify-around lg:flex flex-row lg:text-12px">
+        <div className=" basis-2/3 flex flex-row items-center justify-around sm: mt-8 mr-3 mb-5 lg:m-0">
           <div className="flex flex-row items-center justify-start uppercase ">
             <Image alt="Icon" height={5} priority src="/images/1.png" width={10} />
             <div className="ml-2">
@@ -51,7 +51,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="flex flex-row items-center space-x-3 basis-6/12 sm:mt-10 ml-10 lg:m-0">
+        <div className="flex flex-row items-center space-x-3 basis-6/12 sm:mt-10 ml-5 lg:m-0">
           <div className="flex flex-row  items-center basis-5/12 sm:mb-8 md:mb-7 lg:m-0">
             <Image alt="Icon phone" height={5} priority src="/images/icon_phone.png" width={15} />
             <div className="ml-3">+38 032 258-22-82</div>
