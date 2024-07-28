83f9c63ca8bda337c7aed837b897c196a42043cb
d_yermakov <gntnit@gmail.com>
2024-07-28 15:43:01 +0300

    fixes after merge

--- src/components/main_page/main-page-content.tsx
+++ src/components/main_page/main-page-content.tsx
@@ -1,41 +1,6 @@
 import Image from 'next/image'
 import type { MainPageContentInterface } from '@/interfaces/main-page.interface'
 
-// todo get this data from server
-const mainPageData = {
-  title: 'Кафедра ІСМ готує професіоналів для відомих ІТ-компаній та корпорацій',
-  items: [
-    {
-      id: 1,
-      image: '/images/bg/main_news_1.svg',
-      title: 'Динамічне академічне життя',
-      descriptionShort: 'Наш факультет комп\'ютерних наук прагне створити студентам оптимальні умови для навчання та досліджень. Оновлені програми курсів, наближені до реальної динаміки ІТ-сфери, дозволяють студентам отримувати актуальні знання та вміння. Крім того, ми заохочуємо активну участь студентів у конференціях, семінарах та воркшопах.',
-    },
-    {
-      id: 2,
-      image: '/images/bg/main_news_2.svg',
-      title: 'Практичний досвід через реальні проєкти',
-      descriptionShort: 'Факультет комп\'ютерних наук зосереджений на забезпеченні студентів реальними можливостями для практичного застосування знань. Студенти мають доступ до роботи над проектами, що відображають найновіші тенденції та виклики в ІТ-сфері, роблячи їх набагато краще підготовленими до реального професійного світу.',
-    },
-    {
-      id: 3,
-      image: '/images/bg/main_news_3.svg',
-      title: 'Активне громадське життя',
-      descriptionShort: 'Життя на факультеті не обмежується лише навчанням. Велика кількість студентських організацій та волонтерських програм дозволяє студентам створювати різноманітне дозвілля, розвивати лідерські здібності та отримувати незабутні спогади.',
-    },
-  ],
-}
-function sliceStr(str: string, count = 100) {
-  if (!str?.length) {
-    return '...'
-  }
-  if (str.length < count) {
-    return str
-  }
-  return `${str.slice(0, count)}...`
-}
-export default function MainPageContent() {
-
 interface Props {
   mainPageContent?: MainPageContentInterface | null
 }
@@ -51,9 +16,9 @@ export default function MainPageContent({ mainPageContent }: Props) {
               mainPageContent?.mainItems.map((item, _index) => (
                 <div className="grid gap-8 grid-rows-[156px_50px_1fr] border-b-8 border-green pb-4" key={item.id}>
                   <div className="flex justify-center items-end">
-                    <Image alt={item.title} className="max-h-[153px] w-auto md:h-auto md:w-full" height={100} src={item.img} width={166} />
+                    <Image alt={item.itemTitle} className="max-h-[153px] w-auto md:h-auto md:w-full" height={100} src={item.img} width={166} />
                   </div>
-                  <h4 className="font-bold text-xl card-title-line text-update-blue">{item.itemTitle}</h4>
+                  <h3 className="font-bold text-xl card-title-line text-update-blue">{item.itemTitle}</h3>
                   <p className="font-medium text-lg self-stretch">{item.text}</p>
                 </div>
               ))
