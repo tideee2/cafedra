2d824db7694f7e97dbb4ffbfe48c1f878f87ef6c
d_yermakov <gntnit@gmail.com>
2024-07-29 15:51:32 +0300

    fixes to layout to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,14 +12,14 @@ import Footer from '@/components/footer'
 
 export default function notFound() {
   return (
-    <body className="flex flex-column w-full h-full bg-white">
+    <main className="flex min-h-screen flex-col bg-update-bg" id="content">
       <Header />
       <div className="w-full h-52 text-lg font-bold bg-custom-gray1 p-12">404 - Сторінка не знайдена</div>
       <div className="w=full h-60 text-lg font-bold text-custom-blue1 p-12 underline bg-custom-gray1">
         <Link href="https://cafedra-neon.vercel.app/">Повернутися на головну сторінку</Link>
       </div>
+      <div className="flex-grow"></div>
       <Footer />
-    </body>
-
+    </main>
   )
 }
