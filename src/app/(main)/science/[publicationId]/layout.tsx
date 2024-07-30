c77fd5f1802def1ee1b417e6bada2fe1d027153a
d_yermakov <gntnit@gmail.com>
2024-07-30 15:58:52 +0300

    fixes for og tags for science page

--- src/app/(main)/science/[publicationId]/layout.tsx
+++ src/app/(main)/science/[publicationId]/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react'
 import React from 'react'
 import type { Metadata } from 'next'
 import { CONFIG } from '@/constants/config'
+import type { SciencePublication } from '@/interfaces/science'
 
 interface Props {
   params: { publicationId: string }
@@ -9,13 +10,17 @@ interface Props {
 export async function generateMetadata(
   { params }: Props,
 ): Promise<Metadata> {
-  const publication = await fetch(`${CONFIG.api.publications}/${Number(params.publicationId)}`).then(res => res.json())
+  const publication: SciencePublication = await fetch(`${CONFIG.api.publications}/${Number(params.publicationId)}`)
+    .then(res => res.json())
 
+  const title = publication.title ? `Публікація: ${publication.title}` : 'Публікація'
   return {
-    title: publication?.title || '',
+    title,
     openGraph: {
-      title: publication?.title || '',
-      description: publication?.description || '',
+      title,
+      description: publication?.content || '',
+      url: 'https://cafedra-neon.vercel.app/',
+      images: '/images/new_main_bg.png',
     },
   }
 }
--- src/app/(main)/science/layout.tsx
+++ src/app/(main)/science/layout.tsx
@@ -4,6 +4,13 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Усі публікаці факультету',
+  openGraph: {
+    title: {
+      template: '%s | Кафедра інформаційних систем та мереж',
+      default: 'Кафедра інформаційних систем та мереж',
+    },
+    images: '/images/new_main_bg.png',
+  },
 }
 export default function SearchLayout({
   children,
