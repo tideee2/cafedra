bbeafa9c1e17fdefdd77b71b9e63c403dd6f36c5
d_yermakov <gntnit@gmail.com>
2024-07-30 14:10:38 +0300

    changes for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,26 @@
-'use client'
-
 import React from 'react'
 
 import Link from 'next/link'
 
 import '@/app/globals.css'
 
+import { headers } from 'next/headers'
 import Header from '@/components/header/header'
 
 import Footer from '@/components/footer'
 
-export default function notFound() {
+export default function NotFound() {
+  const headersList = headers()
+  const referer = headersList.get('Referer')
   return (
     <main className="flex min-h-screen flex-col bg-update-bg" id="content">
       <Header />
-      <div className="w-full h-52 text-lg font-bold bg-custom-gray1 p-12">404 - Сторінка не знайдена</div>
       <div className="w=full h-60 text-lg font-bold text-custom-blue1 p-12 underline bg-custom-gray1">
         <Link href="https://cafedra-neon.vercel.app/">Повернутися на головну сторінку</Link>
+      </div>
+      <div className="w-full h-52 text-xl bg-custom-gray1 p-12">Нажаль
+        <span className="font-bold">, { referer }
+        </span> - Сторінка не знайдена
       </div>
       <div className="flex-grow"></div>
       <Footer />
